319906ab2fae50cfa5481ca8d0aafc3aeaad27aa
Cruzdal <54964618+Cruzdal@users.noreply.github.com>
2019-09-10 12:40:59 -0400

    Add files via upload
    Added a function that takes the given arguments and powers them by k. Testing different methods to determine how to find armstrong numbser

--- 1.1.c
+++ 1.1.c
@@ -10,10 +10,27 @@ for the power that was entered and within the range
 
 
 
-int main (int argc, char *argv[]) {
-   int k, q, p;
+int main (int argc, char *argv[])//takes arguments from command line
+{
+   int k, q, p, tot;
 
-   printf("Input a power, minimum range and a max range\n");
+   printf("Question: %s",argv[0]);
+   printf("\nInput a power, minimum range and a max range\n");
    scanf("%d%d%d",&k, &q, &p);
 
+   tot = armstrong(k,q,p);
+
+   printf("%d\n", tot);
+}
+
+
+int armstrong(int k, int q, int p)//calculates the armstrong number
+{
+    int sum = k + q +p;
+    int l;
+    for (l=q; l<=p; l++)
+    {
+    sum += pow(l,k);
+    }
+    return sum;
 }
