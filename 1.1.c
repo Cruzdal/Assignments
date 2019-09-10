5f5457fd1c54dcff9aa8aa4826cc00da58067b4d
Cruzdal <54964618+Cruzdal@users.noreply.github.com>
2019-09-10 11:52:30 -0400

    Add files via upload
    The starting of question 1.1c. This commit is just a simple block of code that grabs command line arguments

---
+++ 1.1.c
@@ -0,0 +1,19 @@
+/*
+The user inputs 3 numbers, a power and a range.
+The program will then output the armstrong numbers
+for the power that was entered and within the range
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main (int argc, char *argv[]) {
+   int k, q, p;
+
+   printf("Input a power, minimum range and a max range\n");
+   scanf("%d%d%d",&k, &q, &p);
+
+}
