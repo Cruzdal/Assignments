71b15f7d6ab137b4102b1e48d73b7ee5e93c8f31
Cruzdal <54964618+Cruzdal@users.noreply.github.com>
2019-09-11 13:26:10 -0400

    Add files via upload
    Re-formatted the blocks of code, added more comments, type casted some variables, added a counter to determine the length of the digits,  and other various things

--- 1.1.c
+++ 1.1.c
@@ -7,37 +7,42 @@ for the power that was entered and within the range
 #include <stdio.h>
 #include <math.h>
 
-
-
-int main (int argc, char *argv[])//takes arguments from command line
-{
-   int k, q, p;
-
-   scanf("%d%d%d",&k,&q,&p);//power, min, max range
-   armstrong(k,q,p);
-}
-
-
 void armstrong(int k, int q, int p)//calculates the armstrong number.
 {
     int a;
 
     for (a = q; a <= p; a++)
     {
-        int sum, num, rem, count;
+        int sum, num, rem, count; //sum to add powers, num as a num, rem is the remainder used to get the decimal to power
+
         sum = 0;
+        count = 0;
+        float var = 0;
         num = a;
 
         while(num != 0)
         {
-        rem  = num % 10;    //gets the remainder
-        sum += pow(rem,k);  //powers the remainder by k
-        num /= 10;          //shifts the number
+            rem  = num % 10;    //gets the remainder
+            var = pow(rem,k);   //powers the remainder by k
+            sum += (int)var;    //type casts var into an int
+            num /= 10;          //shifts the number
+            count++;            //used to count the amount of digits
         }
 
-        if(sum == a)//if the summed power is an armstrong
+        if(sum == a && count == k)//if the summed power is an armstrong AND if the length is the same as the power
         {
-        printf("%d\n", sum);
+            printf("%d", sum); // prints out the armstrong numbers
+
         }
     }
+}
+
+
+int main (int argc, char *argv[])//takes arguments from command line
+{
+    int k = atoi (argv[1]),
+        p = atoi (argv[2]),
+        q = atoi (argv[3]);
+
+    armstrong(k,p,q);
 }
