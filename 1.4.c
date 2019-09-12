5243eb821077539928ffea7188db034707372679
PateJash <54955181+jashpatel4533@users.noreply.github.com>
2019-09-11 20:52:39 -0400

    First Commit
    Question 4: Committed by Jash Patel

---
+++ 1.4.c
@@ -0,0 +1,48 @@
+/*
+* Name: Jash Patel
+* Assignement 1: question 1.3c
+* Summary: This program is designed to remove all the occurances of a character from second string  in the string
+*/
+#include <stdio.h>
+#include <stdlib.h>
+//#include <string.h>
+
+void rmstr(char str[],char str2[]){
+
+  
+  int count =0,count2=0; 
+ 
+    int incrementStr2= str2[count];
+
+	char *ptr=str; //points to the value of 0th elementx	
+    char *ptr2=str2;
+  
+       for( ptr=str;*ptr!='\0';ptr++){
+ 			//for (int i = 0; i < strlen(str2); i++) {
+        	if(*ptr!=*str2){          // check if pointer is not pointing to the occurances of the character
+        	
+			 printf("%c", *ptr);       // Print non occuraces characters in a string
+			 
+		}
+		//incrementStr2=str2[count++];
+	}
+  }
+
+
+
+int main(){
+	char userInput1 [1000];
+	char userInput2 [1000];
+	printf("Please enter the first String: \n");
+	scanf("%[^\n]%*c",userInput1); // takes the user input input until newline
+	printf("Please enter the second String:\n");
+	scanf("%[^\n]%*c",userInput2);
+	 //char twoStr [200];
+   //memcpy(userInput1, userInput2, sizeof(userInput2));
+  // printf("After memcpy dest = %s\n", userInput1);
+     
+     rmstr(userInput1 , userInput2);
+
+	
+	
+}
