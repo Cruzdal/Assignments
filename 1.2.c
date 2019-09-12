6aa853f8c3ec5261d4f54a99cb566404bc320f82
Cruzdal <54964618+Cruzdal@users.noreply.github.com>
2019-09-11 23:34:26 -0400

    Add files via upload
    Re wrote majority of the code. Added a Phi function that counts how many GCD's were 1 and prints that number,
    the GCD function is basically now a swapping function, added a loop in the main function so that the user can repetitively enter numbers

--- 1.2.c
+++ 1.2.c
@@ -1,15 +1,16 @@
 /*
+Written by: Dallas Cruz
 The user inputs a series of numbers and the program will give
 euler's phi of them.
 
 */
 #include <stdio.h>
-#include <math.h>
+
 /*
 To program this, I can calculate the GCD and write a function for euclidean algorithm
 then use another function that takes the gcd and determines if they are one
 conditional loop ie while (x!=0) for gcd. Mod the number until it reaches 0.
-gcd requires 1 nums, p-1 and loop through those numbers. Count numbers that have a GCD of 1
+gcd requinum 1 nums, p-1 and loop through those numbers. Count numbers that have a GCD of 1
 ie
             15
 [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
@@ -18,49 +19,45 @@ ie
 
 */
 
-void gcd (int p)
+int gcd (int q, int p)
 {
-    int q, rem,result = 0, length=0, count=0;
-   // rem = p % q;
-    for(q = 1; q < p; q++)
+    int rem = q % p; //intially mods the first two numbers
+    int num = 0;
+
+    while ( rem != 0 )//euclidean algorithem. basically a swapping method but adds in a mod in it
+        {
+            num = rem;      //takes the first mod
+            rem = p % rem;  //mods the prev with the next numuber p
+            p = rem;        //p becomes the new remainder and repeats until the final number is 0
+        }
+
+    return num;
+}
+
+int phi (int q, int p)//gets the value of the relative prime numbers
+{
+    int count = 0;
+
+    for (p = 1; p < q; p++)
     {
-            length++;
-            do{
-            result = rem;
-            rem = q % rem;
-            q = rem;
-            } while(rem !=0);
-
-            if(rem == 1)
-            {
-                count++;
-            }
-
-    printf("%2d %d %d \n", length,rem,count);
+    if(gcd(q,p) == 1)//checks to see if the GCD is 1
+        {
+            count++;//raises count and displays it as the phi
+        }
     }
 
+    return count;
 
 }
 
-
-int main (int argc, char *argv[])//takes arguments from command line
+int main(int argc, char *argv[])
 {
-   //int e = atoi (argv[1]);
-   int p,q;
-   scanf("%d", &p);
-   gcd(p);
+	int p,i;
+    for(i= 1; i <argc;i++)
+    {
+        int q = atoi (argv[i]);
+
+        printf("Phi of %d is %d\n",q, phi(q,p));
+    }
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
