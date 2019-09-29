aa5e726654fdddba0b0b99f480a2514f9c2637bc
Cruzdal <54964618+Cruzdal@users.noreply.github.com>
2019-09-28 21:21:45 -0400

    Add files via upload
    Basic libraries and main

---
+++ filecopy.c
@@ -0,0 +1,21 @@
+/*
+Written by: Dallas Cruz
+This program takes a text file and copies the content in it into another test file
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (int argc, char *argv[])
+{
+
+    char buffer[200];
+    char cBuffer[200];
+
+    int pipeId[2];
+
+
+    return 0;
+
+}
