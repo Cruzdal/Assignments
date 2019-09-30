c6c17bca330706f35469390b7acb6bd3ad28bfd8
PateJash <54955181+jashpatel4533@users.noreply.github.com>
2019-09-30 00:02:11 -0400

    Second Commit
    By: Jash Patel

--- EchoClient.java
+++ EchoClient.java
@@ -6,39 +6,28 @@ public class EchoClient {
 	private static String localhost;
 
 	public static void main(String[] args) {
+		 try  {
+			 localhost= args[0];
+			}
+			catch (Exception e){
+	                 System.out.println("Usage:java Echoclient localhost");
+			}
 
-		// localhost= args[0];
 		try {
 
 			Socket connection = new Socket(localhost, 50);
-			// DataInputStream dataInput = new DataInputStream(connection.getInputStream());
 			InputStream dataInput = connection.getInputStream();
 			OutputStream dataOutput = connection.getOutputStream();
-			// InputStream inputStr = connection.getInputStream();
 			Scanner sc = new Scanner(System.in);
-			// InputStreamReader clientInput = new InputStreamReader(System.in);
 
-			// BufferedReader buffer = new BufferedReader(clientInput);
-			// BufferedWriter buffWriter = new BufferedWriter(buffWriter);
-			// Read the user input string
-
-			// String input = clientStr.toString();
+		
 			while (true) {
 				String readInput = sc.nextLine();
 
 				byte[] buffer = readInput.getBytes();
-				// String convertToString = new String(buffer);
-				// char c= sc.next().equals(".");
-				// System.out.println(c);
-				// System.out.println(convertToString);
-
-				// dataOutput.flush();
-				// for (int x=0; x<readInput.length();x++) {
-				// byte[] buffer = readInput.charAt(x)getBytes();
-
-				// dataOutput.write(buffer);
+		
 				if (readInput.equals(".")) {
-					// System.out.println("hi");
+	
 					connection.close();
 					break;
 
@@ -60,26 +49,11 @@ public class EchoClient {
 				}
 			}
 
-			// byte[] serverStr = new byte[1024];
-			/*
-			 * String strOut=""; String strIn=""; while (!strOut.equals(".")) { strOut=
-			 * inputStr.readLine(); dataOutput.writeUTF(strOut);
-			 * //dataOutput.write(strOut.getBytes()); //dataOutput.flush();
-			 * strIn=dataInput.readUTF(); System.out.println("Server: "+strIn);
-			 */
-			/*
-			 * int strIn = dataInput.read(serverStr, 0, serverStr.length); while(strIn
-			 * !=-1){ System.out.println("Server: "); buffer. }
-			 * System.out.println("Server: "); dataOutput.write(readOutput);
-			 * 
-			 */
-
-			// }
 
 			dataInput.close();
 			dataOutput.close();
 			connection.close();
-			// }
+
 		} catch (IOException e) {
 			System.err.println("Client Disconnected");
 
--- EchoServer.java
+++ EchoServer.java
@@ -7,7 +7,7 @@ public class EchoServer {
 
 		try {
 
-			ServerSocket serverSocket = new ServerSocket(50);
+				ServerSocket serverSocket = new ServerSocket(50);
 
 			
 				System.out.println("server waiting, use cntl-C to quit\n");
@@ -15,14 +15,8 @@ public class EchoServer {
 				System.out.println("server connected");
 				
 				InputStream dataInput = clientSocket.getInputStream();
-				
-				//DataInputStream dataInput = new DataInputStream(clientSocket.getInputStream());
 				OutputStream dataOutput = clientSocket.getOutputStream();
-				//InputStreamReader clientInput = new InputStreamReader(System.in);
-	          //  BufferedReader buffer = new BufferedReader(clientInput);
-	           
-				//Scanner sc = new Scanner(System.in);
-	            String strIn="",strOut="";
+			
 	         while (true) {   
 				byte[] buffer = new byte[1024];
 			
@@ -33,32 +27,14 @@ public class EchoServer {
 				if(readInput !=-1) {
 					 convertToString = new String(buffer);
 					System.out.println(convertToString);
-					//strOut=buffer.readLine();
-					
+			
 				}
 				else {
 					break;
 				}
 				dataOutput.write(buffer);
 				dataOutput.flush();
-				
-				//int readInput = dataInput.read(inputStr, 0, inputStr.length);
-			//	int writeOutput = serverOutput.write(inputStr,0,inputStr.length);
-				//System.out.println(Integer.toString(readInput));
-				/*while (!strIn.equals(".")) {
-					strIn = dataInput.readUTF();
-					System.out.println(strIn);
-					strOut=buffer.readLine();
-					System.out.println(strOut);
-
-					dataOutput.writeUTF(strIn);
-					dataOutput.flush();
-					
-					
-				/*	if (readInput == -1) {
-						break;
-					}*/
-				//}
+			
 			
 				dataInput.close();
 				clientSocket.close();
