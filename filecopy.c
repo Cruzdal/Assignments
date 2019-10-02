bf70b693bc1f7676f5eb164d3d22c6405ca8e599
Cruzdal <54964618+Cruzdal@users.noreply.github.com>
2019-10-02 16:53:18 -0400

    Add files via upload
    Added a lot of things. Mainly 3 if statements that reads from the original file, writes into the copied file, and an if statement to output a problem if one has occurred.

--- filecopy.c
+++ filecopy.c
@@ -2,7 +2,6 @@
 Written by: Dallas Cruz
 This program takes a text file and copies the content in it into another test file
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>  //used for ssize_t which is used to count bytes
@@ -11,28 +10,49 @@ This program takes a text file and copies the content in it into another test fi
 
 int main (int argc, char *argv[])
 {
-
     char buffer[200];
     char cBuffer[200];
-
     int pipeId[2];
+    pid_t cId;
 
     if (argc != 3)     //if there isnt 3 arguments
     	{
-
       		perror("Filecopy needs 3 arguments. \n");
       		exit(1);
     	}
 
-        char* original = argv[1];
-        char* copied = argv[2];
+        int original = open(argv[1],O_RDWR, 0666);                                          // opens file to read and write, 0666 are the perms used which is read and write
+        int copied = open(argv[2], O_CREAT | O_RDWR, 0666);                                // opens file create, read and write into target file, 0666 are the perms used which is read and write
+        ssize_t cFile, length;
 
-        close(pipeId[1]);                                                            // end the writing of the pipe
+        pipe(pipeId);
 
-        ssize_t cFile = read(pipeId[0], cBuffer, sizeof(cBuffer));                  // reads the contents of pipe and puts it into buffer
+        if (fork() ==0)
+        {
 
-        close(pipeId[0]);                                                          // end the reading of the pipe
+            close(pipeId[1]);                                                            // end the writing of the pipe
+            cFile = read(pipeId[0], cBuffer, sizeof(cBuffer));                          // reads the contents of pipe and puts it into buffer
+            close(pipeId[0]);                                                          // end the reading of the pipe
 
+            write(copied, cBuffer, cFile);                                           // writes the contents of pipe and puts it into child buffer
+        }
+
+        else if (cId == -1)
+        {
+
+            perror("There was a problem");
+            exit(1);
+        }
+
+        else
+        {
+            close(pipeId[0]);                                                    // end the reading of the pipe
+
+            length = read(original, buffer, sizeof(buffer));                   // get number bytes to read
+
+            write(pipeId[1], buffer, length);                                // writes the contents of pipe into child
+            close(pipeId[1]);                                               // end the writing of the pipe
+        }
     return 0;
-
 }
+
