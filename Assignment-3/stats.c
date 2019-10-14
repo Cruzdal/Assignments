26f3ec50a057ea8163ea0032985c175c61f4456a
Cruzdal <54964618+Cruzdal@users.noreply.github.com>
2019-10-14 10:38:54 -0400

    Main Method and Functions
    Added a description of what the file does, and added names for the functions

---
+++ Assignment-3/stats.c
@@ -0,0 +1,35 @@
+/*
+Written by: Dallas cruz
+This program uses multithread to calculate various stat values for a list of numbers.
+First thread will find the average
+Second thread will find the maxmimum
+The last thread will find the minimum
+*/
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int count = 0;
+int avg;
+int max;
+int min;
+
+void *average(void *arg)
+{
+
+}
+
+void *maximum(void *arg)
+{
+
+}
+
+void *minimum(void *arg)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+
+}
