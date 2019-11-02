146f154f55b3bcfdc2c1400d7be3321531508772
Cruzdal <54964618+Cruzdal@users.noreply.github.com>
2019-11-02 18:15:22 -0400

    Added Buffer, Consumer and Producer methods
    Followed the skeleton code/outline as shown on the slate documents for the buffer, consumer and producer

--- Project/semi.c
+++ Project/semi.c
@@ -10,6 +10,57 @@ Once awake, will attempt to remove an item from the buffer
 #include <pthread.h>
 #include <unistd.h>
 
+typedef int buffer item;
+#define BUFFER SIZE 5
+
+void *producer (void *param)
+{
+   buffer item item;
+
+   while (true)
+   {
+       sleep(rand());
+
+       item = rand();
+
+       if (insert_item(intem))
+       {
+           fprintf("Number could not be inserted into buffer");
+       }
+       else
+       {
+           printf("Producer produced %d\n" ,item);
+       }
+   }
+
+ void *consumer (void *param)
+ {
+     buffer item item;
+
+     while (true)
+     {
+         sleep(rand());
+
+         if (remove_item(&item))
+         {
+             fprintf("Consumer could not remove number");
+         }
+         else
+         {
+             printf("consumer consumed %d\n" ,item);
+         }
+
+     }
+ }
+
+
+
+
+
+
+
+
+}
 
 int main(int argc, char *argv[]) {
 
